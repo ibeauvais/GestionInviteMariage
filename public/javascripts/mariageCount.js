3ae992e12c4d954d52b52484596076fc32a131fb
ibeauvais <zeiphf@yahoo.fr>
2012-05-08 20:36:12 +0200

    Ajout Adulte

---
+++ public/javascripts/mariageCount.js
@@ -0,0 +1,16 @@
+
+
+function enterAddAdulte(){	alert($('#modalAdulte'));
+	$('#modalAdulte').modal('show');
+
+}
+
+function closeAddAdulte(){
+	$('#modalAdulte').modal('hide');
+}
+
+
+
+function addAdulte(idInvite){
+	alert($('#adulteName').val());
+}
